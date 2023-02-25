72f0eb7ee82a06bc1f7fb1c8a803ae8a2c9dd1bf
Patrick Piccini <patrickbpiccini@hotmail.com>
2023-02-24 22:59:37 -0300

    finalized of logica of website

--- src/tamplate/js/script.js
+++ src/tamplate/js/script.js
@@ -1,9 +1,20 @@
+var icon_tags = new Object() // object with selected values
+var icon_list = []
+var counter = 0 
+
+
 function gerarLink() {
+    let object_index = Object.keys(icon_tags)
+    object_index.forEach((index) => {
+        icon_list.push(icon_tags[index]["value"])
+    });
+
+
     var icon = document.getElementById("select_icon").value;
     var perline = document.getElementById("perline").value;
     var size = document.getElementById("size").value;
     var theme = document.getElementById("theme").value;
-    var link = "http://www.exemplo.com.br/icons?icon=" + icon
+    var link = "http://localhost/icons?icon=" + icon_list.toString()
 
     if(size){
         link += "&size="+size
@@ -17,8 +28,6 @@ function gerarLink() {
     $("#colum_div_1").empty()
     $("#colum_div_1").append(`<label class="label_text" for="tema">Preview:</label><img id="preview" src="" alt="">`)
     
-    link="https://skillicons.dev/icons?i=js,html,css,wasm,python,js,html,css,wasm,python,js,html,css,wasm,python,js,html,css,wasm,python,css,wasm,python,css,wasm,python,css,wasm,python&perline=7"
-
 
     console.log(link)
     var linkGerado = document.getElementById("link-gerado");
@@ -36,3 +45,29 @@ function copy_textLink() {
         alert(`Has been copyed the link to use:\n${text_link}`)
 }
 
+
+function attualizeLabels() {
+    var selecionador = document.getElementById("select_icon");
+    var tag = document.createElement("label");
+    var key = counter+=1
+    var value = selecionador.value
+
+    // Add the seletionaded options in object of icons
+    icon_tags[key] = {value};
+
+    tag.textContent = selecionador.value+' X';
+    tag.className = key
+    tag.id = "label_tag"
+
+
+    tag.addEventListener("click", function() {
+        // Delete the seletionaded options in object of icons
+
+        delete icon_tags[this.className]
+        
+        this.parentNode.removeChild(this);
+      });
+
+    var tagsDiv = document.getElementById("div_tags");
+    tagsDiv.appendChild(tag);
+  }
