fd2b880ff1cc6352d5a90abcebd9bc36153576e5
Patrick Piccini <patrickbpiccini@hotmail.com>
2023-03-19 22:03:44 -0300

    try to do something with gitactions

--- conf/gunicorn.conf.py
+++ conf/gunicorn.conf.py
@@ -1,8 +1,8 @@
 # gunicorn.conf.py
 bind = "localhost:8000"
 
-accesslog = "/home/ubuntu/app/devicons/logs/devicon.access.log"
-errorlog = "/home/ubuntu/app/devicons/logs/devicon.error.log"
+accesslog = "/home/ubuntu/actions-runner/_work/devicons/deviconslogs/devicon.access.log"
+errorlog = "/home/ubuntu/actions-runner/_work/devicons/devicons/logs/devicon.error.log"
 
 capture_output = True
 enable_stdio_inheritance = True
